db84bf7918492f6a1edad36342095bfabf1443af
TheDeath <th3d34th777@gmail.com>
2024-08-13 01:43:28 -0400

    Actu del Salir del Menu Principal

--- Estadisticas.py
+++ Estadisticas.py
@@ -1,9 +1,4 @@
 class Estadisticas():
-   
-   def __init__(self, vitalidad, defensa, suerte):
-      self.vitalidad = vitalidad
-      self.defensa = defensa
-      self.suerte = suerte
-      vitalidad = 0
-      defensa = 0
-      suerte = 0
+   vitalidad = 0
+   defensa = 0
+   suerte = 0
--- Inicio.py
+++ Inicio.py
@@ -1,23 +1,30 @@
 from Personaje import Personaje
 from Estadisticas import Estadisticas
+import time
 
 def menu_inicio():
    print("\nBienvenido a Tragones y Mazmorras")
    print("\n1 - Iniciar Juego")
    print("\n0 - Salir")
    try:
-      opcion = int(input("\nSeleccione una opcion : "))         
+      opcion = int(input("\nSeleccione una opcion : "))       
    except:
       print("\nOpcion Invalida, intentelo de nuevo")
+      menu_inicio()
    else:
-      print("Crear Personaje")
-      nombre = input("Nombre de su Personaje: ")
-      edad = input("Ingrese la edad: ")
-      clase= Clases()
-      profesion= Profesion()
-      arma= Armas()
-      nivel=Level()
-      stats=Stats_Base()
+      if(opcion == 0):
+         print("Saliendo del juego... ")
+         time.sleep(3)
+         exit
+      elif(opcion == 1):
+         print("Crear Personaje")
+         nombre = input("Nombre de su Personaje: ")
+         edad = input("Ingrese la edad: ")
+         clase= Clases()
+         profesion= Profesion()
+         arma= Armas()
+         nivel=Level()
+         stats=Stats_Base()
 
 def Clases():
    while True:
@@ -30,37 +37,46 @@ def Clases():
       else:
          if clase == 1:
             clasefin= 'Mago'
+            print("Bienvenido sea erudito arcano y conocedor de toda magia")
             break
          elif clase == 2:
             clasefin= 'Paladin'
+            print("Me inclino ante usted y su honor caballero sagrado")
             break
          elif clase == 3:
             clasefin= 'Guerrero'
+            print("Que su gran fortaleza sea la suficiente para su causa valiente guerrero")
             break
          elif clase == 4:
             clasefin= 'Picaro'
+            print("Es hora de desvanecerse y ser uno con las sombras")
             break
          elif clase == 5:
             clasefin= 'Arquero'
+            print("Preparado para poner esas flechas sobre sus enemigos mi estimado francotirador")
             break
          elif clase == 6:
             clasefin= 'Brujo'
+            print("Sin importar los motivos, demuestrale al mundo la capacidad de tu poder")
             break
          elif clase == 7:
             clasefin= 'Monje'
+            print("Demuestrale al mundo que con los puños tienes la capacidad de derrotar hasta dragones")
             break
          elif clase == 8:
             clasefin= 'Gran Sabio'    
+            print("Ehh si escogistes esto es o porque eres un troll o porque le sabes a los easter-eggs :b")
             break
          else:
             print("Opcion no Valida")
+            return Clases()
    return clasefin
 
 def Profesion():
    while True:
       print("Profesiones: 1- Herrero | 2-Medico | 3- Escriba | 4= Alquimista | 5- Profeta ")      
       try:
-         profesion= input("Escoja una profesion")
+         profesion= input("Escoja una profesion ")
       except:
          print("Opcion Invalida")
       else:
@@ -89,20 +105,20 @@ def Profesion():
 
 
 def Armas(clase):
-   daño_arma= 0
+   int(daño_arma)
    while True:
       
       if clase== 'Mago':
          weapon= input("Confirme su arma para continuar")
          arma= "Baston de Madera"
          print("Ha obtenido un "+ arma)
-         daño_arma= 6
+         daño_arma = 6
          break
       elif clase== 'Brujo':
          weapon= input("Confirme su arma para continuar")
          arma= "Baston de Madera"
          print("Ha obtenido un "+ arma)
-         daño_arma= 6 
+         daño_arma = 6 
          break
       elif clase== 'Guerrero':
          try:
@@ -114,17 +130,17 @@ def Armas(clase):
             if weapon== 1:
                arma=='Espada de hierro' 
                print("Ha obtenido un "+ arma)
-               daño_arma= 8
+               daño_arma = 8
                break
             elif weapon== 2:
                arma=='Mandoble' 
                print("Ha obtenido un "+ arma)
-               daño_arma= 11
+               daño_arma = 11
                break
             elif weapon== 3:
                arma=='Maza' 
                print("Ha obtenido un "+ arma)
-               daño_arma= 9
+               daño_arma = 9
                break
             elif clase== 'Paladin':
                try:
@@ -136,43 +152,43 @@ def Armas(clase):
                   if weapon== 1:
                      arma=='Espada de hierro' 
                      print("Ha obtenido un "+ arma)
-                     daño_arma= 8
+                     daño_arma = 8
                      break
                   elif weapon== 2:
                      arma=='Mandoble' 
                      print("Ha obtenido un "+ arma)
-                     daño_arma= 11
+                     daño_arma = 11
                      break
                   elif weapon== 3:
                      arma=='Maza' 
                      print("Ha obtenido un "+ arma)
-                     daño_arma= 9
+                     daño_arma = 9
                      break   
             elif clase== 'Arquero':
                weapon= input("Confirme su arma para continuar")
                arma= "Arco de Madera"
                print("Ha obtenido un "+ arma)
-               daño_arma= 6
+               daño_arma = 6
                break   
             elif clase== 'Picaro':
                weapon= input("Confirme su arma para continuar")
                arma= "Cuchillo de hierro"
                print("Ha obtenido un "+ arma)
-               daño_arma= 5
+               daño_arma = 5
                break  
             elif clase== 'Monje':
                print("No obtienes armas pero pegas mas fuerte a puño limpio")
                weapon= input("Confirme para continuar")
                arma= "Puños"
                print("Ha mejorado su daño con los puños")
-               daño_arma= 5
+               daño_arma = 5
                break 
             elif clase== 'Virgen':
                print("Desconozco la razon de por que escogeria esta clase pero weno")
                weapon= input("Confirme su arma para continuar")
                arma= "Palo de Madera"
                print("Ha obtenido un "+ arma)
-               daño_arma= 4
+               daño_arma = 4
                break 
    return arma 
 
@@ -185,50 +201,48 @@ def Level(clase):
    if exp_obtenida == exp_requerida:  
       nivel= nivel+1
       exp_obtenida= 0
-      puntos_hab= 3 + nivel/5
+      puntos_hab= puntos_hab + 3 + nivel/5
       if(clase== 'Virgen'):
-         puntos_hab= 4 + nivel/5
+         puntos_hab= puntos_hab + 4 + nivel/5
          print(f"Felicidades ha subido a Nivel {nivel}")
    elif exp_obtenida > exp_requerida :
       nivel= nivel+1
       exp_obtenida= 0
       if(clase== 'Virgen'):
-         puntos_hab= 4 + nivel/5
+         puntos_hab= puntos_hab + 4 + nivel/5
          print(f"Felicidades ha subido a Nivel {nivel}")
 
 
 def Stats_Base(clase):
    class Base(Estadisticas):
-      def __init__( vitalidad, defensa, suerte):
-         super().__init__(vitalidad, defensa, suerte)
-   while True:  
-      if(clase == 'Mago'):
-         vitalidad = 5
-         defensa = 2
-         suerte = 3
-      elif(clase == 'Paladin'):
-         vitalidad = 6
-         defensa = 4
-         suerte = 0
-      elif(clase == 'Guerrero'):
-         vitalidad = 8
-         defensa = 2
-         suerte = 0
-      elif(clase == 'Brujo'):
-         vitalidad = 4
-         defensa = 2
-         suerte = 4
-      elif(clase == 'Arquero'):
-         vitalidad = 5
-         defensa = 1
-         suerte = 4
-      elif(clase == 'Picaro'):
-         vitalidad = 3
-         defensa = 2
-         suerte = 5
-      elif(clase == 'Virgen'):
-         vitalidad = 3
-         defensa = 1
-         suerte = 6
+      while True:  
+         if(clase == 'Mago'):
+            vitalidad = 5
+            defensa = 2
+            suerte = 3
+         elif(clase == 'Paladin'):
+            vitalidad = 6
+            defensa = 4
+            suerte = 0
+         elif(clase == 'Guerrero'):
+            vitalidad = 8
+            defensa = 2
+            suerte = 0
+         elif(clase == 'Brujo'):
+            vitalidad = 4
+            defensa = 2
+            suerte = 4
+         elif(clase == 'Arquero'):
+            vitalidad = 5
+            defensa = 1
+            suerte = 4
+         elif(clase == 'Picaro'):
+            vitalidad = 3
+            defensa = 2
+            suerte = 5
+         elif(clase == 'Virgen'):
+            vitalidad = 3
+            defensa = 1
+            suerte = 6
 
 menu_inicio()
