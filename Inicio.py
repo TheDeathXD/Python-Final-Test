59943a6f532f76396fe5d4b8b407c116519cdcf2
TheDeath <th3d34th777@gmail.com>
2024-08-23 22:57:02 -0400

    1.3.3

--- Inicio.py
+++ Inicio.py
@@ -2,11 +2,13 @@ from Personaje import Personaje
 from Estadisticas import Estadisticas
 import time
 import main
+
 clasefin = 'Base'
 puntos_hab = 0
-
+nivel = 0
+arma = None
 def menu_inicio():
-   print("\nBienvenido a Tragones y Mazmorras")
+   print("\nBienvenido a Tragones y Mazmorras!")
    print("\n1 - Iniciar Juego")
    print("\n0 - Salir")
    try:
@@ -22,25 +24,35 @@ def menu_inicio():
          time.sleep(3)
          exit
       elif(opcion == 1):
-         print("Crear Personaje")
-         nombre = input("Nombre de su Personaje: ")
+         print("\nCREACION DEL PERSONAJE ")
+         time.sleep(0.8)
+         nombre = input("\nIngrese su nombre: ")
          time.sleep(0.5)
-         edad = input("Ingrese la edad: ")
+         edad = input("\nIngrese la edad: ")
          time.sleep(0.5)
          clase = Clases()
          time.sleep(1)
          profesion = Profesion()
          time.sleep(1)
-         armafin = Armas()
-         time.sleep(1)
-         nivel = Level(nivel)
+         Level()
          stats = Stats_Base()
-         print(f"\nEste es su personaje: \nNombre: {nombre} \nEdad: {edad} años \nClase: {clase} \nProfesion: {profesion} \nArma: {armafin} \nNivel: {nivel} \nStats: \n{stats} ")
+         hoja= [nombre , edad , clase, profesion, arma, nivel , stats]
+         with open("personaje.txt",'a',encoding='UTF-8') as datos:
+            datos.write("\n Hoja de Personaje: ")
+            for i in (hoja):
+               datos.write(f"\n {i} \n")
+            leer= datos.read()
+            print(leer)
+         time.sleep(0.5)   
          opcion1 = input("\nPresione cualquier tecla para aceptar o escriba 'No' para regresar al menú y deshacer el personaje: ")
          if(opcion1 == "No" ):
             return menu_inicio()
          else:
             main.limpiar()
+            with open("GitHub\\Python-Final-Test\\personaje.txt",'a',encoding='UTF-8') as datos:
+               datos.write("\n")
+               for i in range(hoja):
+                  datos.write(f"\n {i} \n")
             print("Bienvenido a Tragones y Mazmorras \nQue la Aventura comience! ")
             time.sleep(2)
             print("Generando terreno... ")
@@ -50,6 +62,8 @@ def menu_inicio():
             print("Generando objetos...")
 
 def Clases():
+   global clasefin
+   global arma
    while True:
       print("Clases: 1-Mago | 2-Paladin | 3- Guerrero | 4-Picaro | 5-Arquero | 6-Brujo | 7-Monje | 8-Virgen")
       try:
@@ -60,39 +74,71 @@ def Clases():
          if clase == "1":
             clasefin= 'Mago'
             print("Bienvenido sea erudito arcano y conocedor de toda magia")
+            time.sleep(0.5)
+            arma = "Baston de Madera"
+            print("Ha obtenido un "+ arma)
+            daño_vel_arma = (6, 1)
             break
          elif clase == "2":
             clasefin= 'Paladin'
             print("Me inclino ante usted y su honor caballero sagrado")
+            time.sleep(0.5)
+            arma = "Espada de Hierro"
+            print("Ha obtenido una "+ arma)
+            daño_vel_arma = (8, 1)
             break
          elif clase == "3":
             clasefin= 'Guerrero'
             print("Que su gran fortaleza sea la suficiente para su causa valiente guerrero")
+            time.sleep(0.5)
+            arma = "Mandoble"
+            print("Ha obtenido un "+ arma)
+            daño_vel_arma = (11, 0.7)
             break
          elif clase == "4":
             clasefin= 'Picaro'
             print("Es hora de desvanecerse y ser uno con las sombras")
+            time.sleep(0.5)
+            arma = "Daga de Hierro"
+            print("Ha obtenido una "+ arma)
+            daño_vel_arma = (5, 1.5)
             break
          elif clase == "5":
             clasefin= 'Arquero'
             print("Preparado para poner esas flechas sobre sus enemigos mi estimado francotirador")
+            time.sleep(0.5)
+            arma = "Arco de Madera"
+            print("Ha obtenido un "+ arma)
+            daño_vel_arma = (6, 1.2)
             break
          elif clase == "6":
             clasefin= 'Brujo'
             print("Sin importar los motivos, demuestrale al mundo la capacidad de tu poder")
+            time.sleep(0.5)
+            arma = "Baston de Madera"
+            print("Ha obtenido un "+ arma)
+            daño_vel_arma = (8, 0.8)
             break
          elif clase == "7":
             clasefin= 'Monje'
             print("Demuestrale al mundo que con los puños tienes la capacidad de derrotar hasta dragones")
+            time.sleep(0.5)
+            arma = "Puños"
+            print(f"Sus {arma} han sido mejorados")
+            daño_vel_arma = (4, 2)
             break
          elif clase == "8":
             clasefin= 'Gran Sabio'    
             print("Ehh si escogistes esto es o porque eres un troll o porque le sabes a los easter-eggs :b")
+            time.sleep(0.5)
+            arma = "Palo de Madera"
+            print("Ha obtenido un "+ arma)
+            daño_vel_arma = (4, 1.5)
             break
          else:
             print("Opcion no Valida")
             return Clases()
-   return clasefin
+   return clasefin, arma, daño_vel_arma
 
 def Profesion():
    while True:
@@ -125,57 +171,10 @@ def Profesion():
             break
    return profesionfin
 
-
-
-
-def Armas():
-   try:
-      print("Su arma correspondiente le será entregada")
-      time.sleep(2)
-   except:
-      print("Error")
-   else:
-      if clasefin== 'Mago' or clasefin== 'Brujo':
-         arma = "Baston de Madera"
-         print("Ha obtenido un "+ arma)
-         daño_arma = 6
-      elif clasefin== 'Guerrero' or clasefin== 'Paladin':
-         print ('1-Espada de hierro | 2-Mandoble | 3-Maza')
-         weapon= input("Escoja su arma: ")
-         if weapon== "1":
-            arma = 'Espada de hierro' 
-            print("Ha obtenido una "+ arma)
-            daño_arma = 8
-         elif weapon== "2":
-            arma = 'Mandoble' 
-            print("Ha obtenido un "+ arma)
-            daño_arma = 11
-         elif weapon== "3":
-            arma = 'Maza' 
-            print("Ha obtenido una "+ arma)
-            daño_arma = 9
-      elif clasefin== 'Arquero':
-         arma = "Arco de Madera"
-         print("Ha obtenido un "+ arma)
-         daño_arma = 6   
-      elif clasefin== 'Picaro':
-         arma = "Cuchillo de hierro"
-         print("Ha obtenido un "+ arma)
-         daño_arma = 5
-      elif clasefin== 'Monje':
-         print("No obtienes armas pero pegas mas fuerte a puño limpio")
-         arma = "Puños"
-         print("Ha mejorado su daño con los puños")
-         daño_arma = 5
-      elif clasefin== 'Gran Sabio':
-         print("Desconozco la razon de por que escogeria esta clase pero weno")
-         arma = "Palo de Madera"
-         print("Ha obtenido un "+ arma)
-         daño_arma = 4
-
-
-def Level(nivel):
-   nivel=1
+def Level():
+   global nivel
+   global puntos_hab
+   nivel = 1
    exp_requerida = nivel * 10
    exp_obtenida = 0
    if exp_obtenida == exp_requerida:  
@@ -194,21 +193,24 @@ def Level(nivel):
 
 
 def Stats_Base():
-   class Bases(Estadisticas): 
+   class Bases(Estadisticas):
       if(clasefin == 'Mago'):
-         general_stats = Estadisticas(5,2,3)
+            general_stats = Estadisticas(5,2,3)
       elif(clasefin == 'Paladin'):
-         general_stats = Estadisticas(6,4,0)
+            general_stats = Estadisticas(6,4,0)
       elif(clasefin == 'Guerrero'):
-         general_stats = Estadisticas(8,2,0)
+            general_stats = Estadisticas(8,2,0)
       elif(clasefin == 'Brujo'):
-         general_stats = Estadisticas(4,2,4)
+            general_stats = Estadisticas(4,2,4)
       elif(clasefin == 'Arquero'):
-         general_stats = Estadisticas(5,1,4)
+            general_stats = Estadisticas(5,1,4)
       elif(clasefin == 'Picaro'):
-         general_stats = Estadisticas(3,2,5)
+            general_stats = Estadisticas(3,2,5)
       elif(clasefin == 'Gran Sabio'):
-         general_stats = Estadisticas(3,1,6)
+            general_stats = Estadisticas(3,1,6)
+
+
+
 
 def LevelUp(estadistica):
    while(puntos_hab>0):
